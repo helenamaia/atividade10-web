23d7bb54dfc9aa49bb0fb967492d90a23fdbf713
helena karol <helenadiasmaia@gmail.com>
2021-04-14 17:19:56 -0300

    mostrando perguntas

--- trabalho.js
+++ trabalho.js
@@ -2,7 +2,7 @@ const urlBase = 'https://opentdb.com';
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 let categorias; 
 let perguntas; 
-
+let vetor = [];
 const elementos = {
     categoriaSelect: document.querySelector('#categoria-pergunta'), 
     telaInicial: document.querySelector('.pagina-opcoes'),
@@ -10,6 +10,12 @@ const elementos = {
     jogar: document.querySelector('.botao-jogar'), 
     dificulty: document.querySelector('#dificuldade-pergunta'),
     category: document.querySelector('#categoria-pergunta'),
+    divPergunta: document.querySelector('.pergunta'),
+    divRespostas: document.querySelector('.radio-respostas'),
+    proximaPergunta: document.querySelector('.next-pergunta'),
+    guardaPergunta: document.querySelector('.save-pergunta'),
+    respondePergunta: document.querySelector('.return-pergunta'),
+    submetePergunta: document.querySelector('.check-pergunta'),
 };
 
 const pegarCategorias = () => {
@@ -36,17 +42,14 @@ const pegarPerguntas = (dificul, categor) => {
     axios.get(`${urlBase}/api.php?amount=10&category=${categor.id}&difficulty=${dificul}`)
       .then(response => {
           
-          const aux = response.data.results;
-          const aux1 = JSON.stringify(aux);
-          perguntas = JSON.parse(aux1);
+          const auxP = response.data.results;
+          const aux1P = JSON.stringify(auxP);
+          perguntas = JSON.parse(aux1P);
           console.log(perguntas);
-          for (let i = 0; i < perguntas.length; i++) {
-              
-            //  colocarCategorias(categorias[i].name);
-              //if(i===(parseInt(categorias.length)-1)){
-                //elementos.categoriaSelect.innerHTML += `<option value="" selected>Selecione uma Categoria</option>`;  
-              //}
-          }
+          colocarPerguntaAleatoria();
+          elementos.proximaPergunta.addEventListener('click', () => colocarPerguntaAleatoria());
+          elementos.guardaPergunta.addEventListener('click', () => guardarPergunta());
+
     });
 }
 
@@ -81,6 +84,12 @@ const pegarPerguntas = (dificul, categor) => {
 
   }
 
+  const guardarPergunta = () => {
+
+
+    colocarPerguntaAleatoria();
+  }
+
   const gerarListaPerguntas = () =>{
     elementos.telaInicial.style.display = 'none';
     elementos.telaJogo.style.display = 'flex';
@@ -108,6 +117,58 @@ const pegarPerguntas = (dificul, categor) => {
     pegarPerguntas(dificul, categor);
     
   }
+
+  const colocarPerguntaAleatoria = () => {
+    
+    
+    let vddFal=false;
+    const i = Math.floor(Math.random() * perguntas.length);
+    const p = perguntas[i];     
+    for (let j = 0; j < vetor.length; j++) {
+      if(i==vetor[j]){
+        vddFal = true;
+      } 
+    }
+
+    if(vetor.length<10){
+      if(vddFal == true){
+        colocarPerguntaAleatoria();
+      }else if(vddFal == false){
+          vetor.push(i);
+          elementos.divPergunta.innerHTML = `<div>${perguntas[i].question}</div>`
+          elementos.divRespostas.innerHTML = ' ';
+          let possibilidades = perguntas[i].incorrect_answers;
+          possibilidades.push(perguntas[i].correct_answer);
+          possibilidades =  possibilidades.sort(() => Math.random() - 0.5)
+          
+          for (let j = 0; j < possibilidades.length; j++) {
+            elementos.divRespostas.innerHTML += `<div><input type="radio" name="respostas" value="${possibilidades[j]}"/>${possibilidades[j]}<br/></div>`
+          }
+
+          elementos.proximaPergunta.addEventListener('click', () => colocarPerguntaAleatoria());
+          elementos.guardarPergunta.addEventListener('click', () => guardarPergunta()); 
+          elementos.respondePergunta.addEventListener('click', () => responderPerguntaGuardada());
+          elementos.submetePergunta.addEventListener('click', () => checarPergunta());         
+
+          
+      }
+    }else if(vetor.length==10){
+      console.log('cabou');
+    }
+    
+
+    
+  }
+
+  const responderPerguntaGuardada = () =>{
+
+  }
+
+  const checarPergunta = () =>{
+    
+  }
+  
+
 
   const sortearCategoriaAleatoria = () => {
       const i = Math.floor(Math.random() * categorias.length);
